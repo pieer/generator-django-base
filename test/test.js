7b8ef2b566211f352ed4fe0388d75e3d8718e873
Marco Santonocito <hello@marcosantonocito.com>
2014-10-06 15:17:34 +0200

    added test file

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*global describe, beforeEach, it*/
+
+var path    = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('Django base generator test', function () {
+    // TODO
+});
